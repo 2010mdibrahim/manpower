22ab9271fc1f7e08e8e889a14001c5a7895b1194
seaum23 <seaum333@gmail.com>
2021-03-04 11:20:48 -0800

    this is from localhost. same changes as server. will pull from server and work on it

--- includes/footer.php
+++ includes/footer.php
@@ -6,34 +6,34 @@
                 <div class="footer-about">
                     <h2>About Us</h2>
                     <p>
-                        Trusted VISA Issuing Agency.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu lectus a leo tristique dictum nec non quam. Suspendisse convallis, tortor eu placerat rhoncus, lorem quam iaculis felis, sed eleifend lacus neque id eros. Integer convallis volutpat neque
                     </p>
                 </div>
             </div>
             <div class="col-md-6 col-lg-8">
                 <div class="row">
                     <div class="col-md-6 col-lg-4">
-                        <!-- <div class="footer-link">
+                        <div class="footer-link">
                             <h2>Services Areas</h2>
                             <a href="">Civil Law</a>
                             <a href="">Family Law</a>
                             <a href="">Business Law</a>
                             <a href="">Education Law</a>
                             <a href="">Immigration Law</a>
-                        </div> -->
+                        </div>
                     </div>
                     <div class="col-md-6 col-lg-4">
-                        <!-- <div class="footer-link">
+                        <div class="footer-link">
                             <h2>Useful Pages</h2>
                             <a href="">About Us</a>
                             <a href="">Practices</a>
                             <a href="">Attorneys</a>
                             <a href="">Case Studies</a>
                             <a href="">FAQs</a>
-                        </div> -->
+                        </div>
                     </div>
                     <div class="col-md-6 col-lg-4">
-                        <!-- <div class="footer-contact">
+                        <div class="footer-contact">
                             <h2>Get In Touch</h2>
                             <p><i class="fa fa-map-marker-alt"></i>123 Street, New York, USA</p>
                             <p><i class="fa fa-phone-alt"></i>+012 345 67890</p>
@@ -45,20 +45,20 @@
                                 <a href=""><i class="fab fa-instagram"></i></a>
                                 <a href=""><i class="fab fa-linkedin-in"></i></a>
                             </div>
-                        </div> -->
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="container footer-menu">
-        <!-- <div class="f-menu">
+        <div class="f-menu">
             <a href="">Terms of use</a>
             <a href="">Privacy policy</a>
             <a href="">Cookies</a>
             <a href="">Help</a>
             <a href="">FQAs</a>
-        </div> -->
+        </div>
     </div>
     <div class="container copyright">
         <div class="row">
