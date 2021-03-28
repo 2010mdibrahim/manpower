d66fd4af11b4414e52e78b2377c1983a75a15740
seaum23 <seaum333@gmail.com>
2021-03-28 19:05:13 +0600

    minor change from server

--- template/listCandidate.php
+++ template/listCandidate.php
@@ -2,9 +2,9 @@
 if(isset($_GET['pp'])){
     $passportNum = base64_decode($_GET['pp']);
     $creationDate = base64_decode($_GET['cd']);
-    $result = $conn -> query("SELECT jobs.jobType, passport.*, DATE(passport.creationDate) as creationDateShow from passport inner join jobs using (jobId) where passport.passportNum = '$passportNum' and passport.creationDate = '$creationDate'");
+    $result = $conn -> query("SELECT jobs.jobType, passport.*, DATE(passport.creationDate) as creationDateShow from passport left join jobs using (jobId) where passport.passportNum = '$passportNum' and passport.creationDate = '$creationDate'");
 }else{
-    $result = $conn -> query("SELECT jobs.jobType, passport.*, DATE(passport.creationDate) as creationDateShow from passport inner join jobs using (jobId) order by passport.creationDate desc");
+    $result = $conn -> query("SELECT jobs.jobType, passport.*, DATE(passport.creationDate) as creationDateShow from passport left join jobs using (jobId) order by passport.creationDate desc");
 }
 ?>
 
