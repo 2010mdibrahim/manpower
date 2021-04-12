9a10ef42ef6ccb84150e7d0bb48d39baf3ac0845
seaum23 <seaum333@gmail.com>
2021-04-12 12:33:23 +0600

    Removed zip temp file

--- template/getZip.php
+++ template/getZip.php
@@ -13,3 +13,4 @@ header('Content-Type: application/zip');
 header('Content-disposition: attachment; filename='.$zipname);
 header('Content-Length: ' . filesize($zipname));
 readfile($zipname);
+unlink($zipname);
