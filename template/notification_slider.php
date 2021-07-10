0696ae7bac8a0c72a3b617f2cd43948305e61586
seaum23 <seaum333@gmail.com>
2021-07-10 15:57:18 +0600

    fixed the notification system

--- template/notification_slider.php
+++ template/notification_slider.php
@@ -1,14 +1,16 @@
 <?php
 include ('database.php');
-$notifications = $conn->query("SELECT passport.fName, passport.lName, passport.finalMedicalStatus, passport.testMedicalStatus, notifications.* from notifications inner join passport on passport.id = notifications.passport_id LEFT JOIN processing on processing.passportNum = passport.passportNum AND processing.passportCreationDate = passport.creationDate where processing.pending not in (2,3) ORDER BY notifications.id desc limit 50");
+$notifications = $conn->query("SELECT processing.processingId, passport.fName, passport.lName, passport.finalMedicalStatus, passport.testMedicalStatus, notifications.* from notifications inner join passport on passport.id = notifications.passport_id LEFT JOIN processing on processing.passportNum = passport.passportNum AND processing.passportCreationDate = passport.creationDate where processing.pending not in (2,3) AND passport.status != 2 ORDER BY notifications.id desc limit 50");
 print_r(mysqli_error($conn));
 $html =    '<ul class="list-group">';
 $i = 0;
 while($notification = mysqli_fetch_assoc($notifications)){
     if(strpos($notification['notification'], 'Medical') !== false){
-        if($notification['finalMedicalStatus'] != 'unfit' AND $notification['testMedicalStatus'] != 'unfit'){
-            $html .= '<li class="list-group-item"><div class="row"><div class="col-md-9">'.$notification['notification'].' - '.$notification['fName'].' '.$notification['lName'].'</div><div class="col-md-3"><span style="float:right"><small>'.$notification['notification_date'].'</small></span></div></div></li>';
-        }
+        if(is_null($notification['processingId'])){
+            if($notification['finalMedicalStatus'] != 'unfit' AND $notification['testMedicalStatus'] != 'unfit'){
+                $html .= '<li class="list-group-item"><div class="row"><div class="col-md-9">'.$notification['notification'].' - '.$notification['fName'].' '.$notification['lName'].'</div><div class="col-md-3"><span style="float:right"><small>'.$notification['notification_date'].'</small></span></div></div></li>';
+            }
+        }        
     }else{
         if(strpos($notification['notification'], 'VISA') !== false){
             $html .= '<li class="list-group-item" style="background-color: #ef5350;color: white;"><div class="row"><div class="col-md-9">'.$notification['notification'].' - '.$notification['fName'].' '.$notification['lName'].'</div><div class="col-md-3"><span style="float:right"><small>'.$notification['notification_date'].'</small></span></div></div></li>';
