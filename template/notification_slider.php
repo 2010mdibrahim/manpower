a84bc9fd3c817771e8b009e60045dd6cf8769f19
seaum23 <seaum333@gmail.com>
2021-07-10 15:28:38 +0600

    minor change

--- template/notification_slider.php
+++ template/notification_slider.php
@@ -7,7 +7,7 @@ $i = 0;
 while($notification = mysqli_fetch_assoc($notifications)){
     if(strpos($notification['notification'], 'Medical') !== false){
         if($notification['finalMedicalStatus'] != 'unfit' AND $notification['testMedicalStatus'] != 'unfit'){
-            $html .= '<li class="list-group-item"><div class="row"><div class="col-md-10">'.$notification['notification'].' - '.$notification['fName'].' '.$notification['lName'].'</div><div class="col-md-2"><span style="float:right"><small>'.$notification['notification_date'].'</small></span></div></div></li>';
+            $html .= '<li class="list-group-item"><div class="row"><div class="col-md-9">'.$notification['notification'].' - '.$notification['fName'].' '.$notification['lName'].'</div><div class="col-md-3"><span style="float:right"><small>'.$notification['notification_date'].'</small></span></div></div></li>';
         }
     }
     $i++;
